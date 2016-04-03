02c6c80236af36a5c97edbbb7c091a39c278b6ba
Sukrit Venkatagiri <sukrit.v.95@gmail.com>
2016-04-03 08:30:14 +0530

    hope

--- scripts/authenticate.py
+++ scripts/authenticate.py
@@ -2,12 +2,17 @@ from functools import partial
 from core import KeystrokeCaptureData
 from model import Fingerprint, FingerprintDatabase
 from sugar import create_fingerprint_from_capture_data
+from sys import argv
+script, username = argv
 DATA_DIR= "./"
 def match_fingerprint(data):
     db= FingerprintDatabase().load_from_dir( DATA_DIR )
     f= create_fingerprint_from_capture_data( "NoName", data )
     best= db.best_match( f )
-    print best.name
+    if username==best.name:
+    	print "Authenticated"
+    else:
+    	print "Imposter!"
 
 def create_object(f):
     data = KeystrokeCaptureData()
---
+++ scripts/train_master_final.py
@@ -0,0 +1,47 @@
+import sys
+import json
+from subprocess import call 
+
+def return_tuples(array) :
+    lst = []
+    size = len(array)
+    for i in range(0,size,3) :
+        lst.append((array[i], array[i+1], array[i+2]))
+        #lst.append(',')
+    return lst
+
+
+
+'''
+for i in range(2,len(sys.argv)) :
+    array2.append(str(sys.argv[i]))
+'''
+import pprint as pp
+with open('./data/data.json') as data_file :    
+    infos = json.load(data_file)
+
+#pp.pprint(data)
+
+print infos
+username = infos['username']
+signature = infos['signature']
+lst = []
+
+for dic in signature :
+    lst.append(dic['keyCode'])
+    lst.append(dic['keyAction'])
+    lst.append(dic['time'])
+
+
+
+lst2 = return_tuples(lst)
+
+print(lst)
+
+
+with open('../data/'+str(username)+'.keypress','w+') as file :
+    file.write(str(lst2).replace("u","").replace("'",""))
+
+#for item in lst2 :
+#    file.write(str(lst2[i]))
+call(["python","train.py",str(username)])
